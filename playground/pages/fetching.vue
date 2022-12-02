57247c00ad0093f0ca5dc54d23adfbfb92692db6
Mahdi Boomeri <mahdi.b2or@gmail.com>
2022-12-02 16:59:32 +0330

    remove useFetch example

--- playground/pages/fetching.vue
+++ playground/pages/fetching.vue
@@ -12,35 +12,4 @@ const { data, fetching } = await useQuery(
     server: false,
   }
 )
-
-const { data: fooData } = await useFetch('/api/foo', {
-  server: false,
-  onResponse({ request, response, options }) {
-    console.log('[fetch response]', {
-      request,
-      response,
-      options,
-    })
-  },
-  onResponseError({ request, response, options }) {
-    console.log('[fetch response error]', {
-      request,
-      response,
-      options,
-    })
-  },
-  onRequest({ request, options }) {
-    console.log('[fetch request]', {
-      request,
-      options,
-    })
-  },
-  onRequestError({ request, options, error }) {
-    console.log('[fetch request error]', {
-      request,
-      options,
-      error,
-    })
-  },
-})
 </script>
