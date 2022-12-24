05a4fff450ac6da8902ccf062f65a47989bd98e1
Mahdi Boomeri <mahdi.b2or@gmail.com>
2022-12-24 21:10:11 +0330

    docs: initial docus integration

---
+++ docs/app.config.ts
@@ -0,0 +1,22 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt Query',
+    description: 'Supercharging Nuxt 3 data fetching.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'mahdi_boomeri',
+      github: 'mahdiboomeri/nuxt-query',
+    },
+    aside: {
+      level: 1,
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'IconNuxtLabs'
+        }
+      ]
+    }
+  }
+})
