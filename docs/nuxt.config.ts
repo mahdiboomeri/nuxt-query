5a6913eba3a38c51031d8a4a626ab0ceb39552f4
Mahdi Boomeri <mahdi.b2or@gmail.com>
2022-12-25 02:44:55 +0330

    fix: trying to fix docs deployment

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,22 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
+  app: {
+    pageTransition: false,
+    layoutTransition: false
+  },
+  modules: ['@nuxtlabs/github-module'],
+  extends: process.env.DOCUS_THEME_PATH || '@nuxt-themes/docus',
+  github: {
+    owner: 'mahdiboomeri',
+    repo: 'nuxt-query',
+    branch: 'main'
+  },
+  colorMode: {
+    preference: 'dark'
+  },
+  build: {
+    transpile: [/content-edge/, /github-module/]
+  },
+  nitro: {
+    preset: 'vercel'
+  }
 })
